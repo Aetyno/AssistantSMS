3fd851029789dd93b24be5fa531e3e0be0dbcfd3
Aetyno <nicolas.barcacci@gmail.com>
2012-10-05 13:40:07 +0200

    Ajout du menus accepter et annuler de l'activité create_sms

--- src/m2/ihm/assistantsms/CreateSMS.java
+++ src/m2/ihm/assistantsms/CreateSMS.java
@@ -2,7 +2,9 @@ package m2.ihm.assistantsms;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class CreateSMS extends Activity {
 
@@ -16,5 +18,30 @@ public class CreateSMS extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_create_sms, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+    	switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                //intent = new Intent(this, Main.class);
+                //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                //startActivity(intent);
+                return true;
+            case R.id.menu_accept:
+            	intent = new Intent(this, Main.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                //startActivity(intent);
+            	return true;
+            case R.id.menu_cancel:
+            	intent = new Intent(this, Main.class);
+                //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            	return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
