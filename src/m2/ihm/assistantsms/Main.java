9f8b86e92a2ed3210ea082931726551170a792bc
Aetyno <nicolas.barcacci@gmail.com>
2012-10-05 12:47:46 +0200

    Ajout des liens des éléments du menu de l'activité main vers les autres activités.

--- src/m2/ihm/assistantsms/Main.java
+++ src/m2/ihm/assistantsms/Main.java
@@ -2,7 +2,9 @@ package m2.ihm.assistantsms;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class Main extends Activity {
 
@@ -16,5 +18,36 @@ public class Main extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	Intent intent;
+    	switch (item.getItemId()) {
+            case R.id.menu_create_sms:
+                // app icon in action bar clicked; go home
+                intent = new Intent(this, CreateSMS.class);
+                //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            case R.id.menu_history:
+            	intent = new Intent(this, History.class);
+                startActivity(intent);
+            	return true;
+            case R.id.menu_history_geo:
+            	intent = new Intent(this, HistoryGeo.class);
+                startActivity(intent);
+            	return true;
+            case R.id.menu_settings:
+            	intent = new Intent(this, Setting.class);
+                startActivity(intent);
+            	return true;
+            case R.id.menu_about:
+            	intent = new Intent(this, About.class);
+                startActivity(intent);
+            	return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
